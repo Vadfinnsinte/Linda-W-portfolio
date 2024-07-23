1406b7f7c9898d104a215ba397c185a0f849df66
Linda Westerlund <lindawesterlund94@gmail.com>
2024-07-23 11:41:17 +0200

    started work on portfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import LandingPage from './components/LanginPage'
+
+function App() {
+  
+
+  return (
+    <body>
+    {/* <h1 className="linda-westerlund-h">Linda Westerlund</h1> */}
+    < LandingPage /> 
+    </body>
+  )
+}
+
+export default App
---
+++ src/components/LanginPage.jsx
@@ -0,0 +1,40 @@
+import profilePic from "../assets/imageLinda.png"
+
+const LandingPage = () => {
+    
+    return (
+        // <div className="landingpage-body">
+        <>
+        
+            {/* <h1 className="linda-westerlund-h">Linda Westerlund</h1> */}
+             <div className="welcome-container">
+                <img className="profile-pic" src={profilePic}/>
+                <section className="welcome-txt">
+                    <h3> Välkommen till min sida!</h3>
+                    <p>Mitt namn är Linda Westerlund och jag studerar till Frontend-developer på NBI/ Handelsakademin.</p>
+                    <p>Här kan du hitta information om mig, min utbildning och projekt som jag jobbat med</p>
+                </section>
+                <section className="lia-container">
+                    <h4 className="lia-head">LIA </h4>
+                    <div className="period-container">
+                        <h4>Period 1</h4>
+                        <p>2024-11-11 </p>
+                        <p> - </p>
+                        <p>2025-01-31 </p>
+                    </div>
+                    <div className="period-container">
+                        <h4>Period 2</h4>
+                        <p>2025-03-17</p>
+                        <p> - </p>
+                        <p>2025-06-06</p>
+                    </div>
+                </section>
+            </div>
+        </>
+
+        // </div>
+    )
+    
+}
+
+export default LandingPage
