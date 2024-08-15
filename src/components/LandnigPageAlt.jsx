da0f8172e37623f4bdc2867447537ad3f29426bb
Linda Westerlund <lindawesterlund94@gmail.com>
2024-08-15 10:13:44 +0200

    made changes to landingPage

--- src/components/LandnigPageAlt.jsx
+++ src/components/LandnigPageAlt.jsx
@@ -2,21 +2,36 @@ import field from "../assets/field.jpeg"
 // import fieldTwo from "../assets/fieldTwo.jpeg"
 import fieldTwo from "../assets/fieldTwoagain.jpg"
 import akira from "../assets/akira.jpeg"
-import profilePic from "../assets/imageLinda.png"
+import profilePic from "../assets/jag.jpg"
 
 const LandingPageAlt = () => {
     return (
         <>
-            <div className="wel-pic">
+            {/* <div className="wel-pic">
                 <img className="field-pic" src={fieldTwo}/>
                 <h1 className="name">Linda Westerlund</h1>
-            </div>
+            </div> */}
             <section className="welcome-txt">
             <img className="profile-pic" src={profilePic}/>
+ 
                 <div>
-                    <h3> Välkommen till min sida!</h3>
-                    <p>Mitt namn är Linda Westerlund och jag studerar till Frontend-developer på NBI/ Handelsakademin.</p>
-                    <p>Här kan du hitta information om mig, min utbildning och projekt som jag jobbat med</p>
+                    <h3> Linda Westerlund - Frontend-utvecklare.</h3>
+                    <p>Välkommen till min portfolio. Här kan man hitta projekt jag har jobbat på, mitt CV och lite mer om mig och min utbildning.</p>
+                    <p>Lite kort om mig, mitt namn är Linda och jag studerar för närvarande till frontend utvecklare på NBI/handelsakademin. Jag bor lite utanför Göteborg med min sambo och vår hund.</p>
+
+                </div>
+                <div className="LIA-container">
+                        <h3>LIA: </h3>
+                    <section className="lia-period">
+                        <h4>Period 1</h4>
+                        <p>2024-11-11</p>
+                        <p>2025-01-31 </p>
+                     </section>
+                    <section className="lia-period">
+                        <h4>Period 2</h4>
+                        <p>2025-03-17</p>
+                        <p>2025-06-06</p>
+                     </section>
 
                 </div>
                 </section>
