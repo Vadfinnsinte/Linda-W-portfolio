d8145a3702b623330f9074fae07e8eb3a6134bf1
Linda Westerlund <lindawesterlund94@gmail.com>
2024-08-16 10:16:38 +0200

    Added classes to landingpage and worked on CSS

--- src/components/LandnigPageAlt.jsx
+++ src/components/LandnigPageAlt.jsx
@@ -14,10 +14,10 @@ const LandingPageAlt = () => {
             <section className="welcome-txt">
             <img className="profile-pic" src={profilePic}/>
  
-                <div>
+                <div className="text">
                     <h3> Linda Westerlund - Frontend-utvecklare.</h3>
                     <p>Välkommen till min portfolio. Här kan man hitta projekt jag har jobbat på, mitt CV och lite mer om mig och min utbildning.</p>
-                    <p>Lite kort om mig, mitt namn är Linda och jag studerar för närvarande till frontend utvecklare på NBI/handelsakademin. Jag bor lite utanför Göteborg med min sambo och vår hund.</p>
+                    <p>Lite kort om mig, mitt namn är Linda och jag studerar för närvarande till frontend utvecklare på NBI/handelsakademin. Jag bor utanför Göteborg med min sambo och vår hund.</p>
 
                 </div>
                 <div className="LIA-container">
@@ -34,6 +34,25 @@ const LandingPageAlt = () => {
                      </section>
 
                 </div>
+                <section className="classes-container">
+                    <h3 className="active-course">Kurser: </h3>
+                <div className="active-course">
+                     <p><span>Fullstackutveckling </span>- Pågående</p>
+                </div>
+                <div className="last-child">
+                    <p><span>HTML och CSS </span>- Avklarad</p>
+                    <p><span>JavaScript grund </span>- Avklarad</p>
+                    <p><span> Grafiska verktyg för gränssnittsdesign </span>- Avklarad</p>
+                    <p> <span>UX och Usability</span> - Avklarad</p>
+                </div>
+                <div className="last-child">
+                    <p><span> Att arbeta i projekt med agila metoder</span> - Avklarad</p>
+                    <p> <span>JavaScript Fördjupning </span>- Avklarad</p>
+                    <p> <span>Testning </span>- Avklarad </p>
+                    <p> <span>LIA </span>- uppkomande</p>
+                    <p><span>Examensarbete </span> - uppkomande</p>
+                </div>
+                </section>
                 </section>
         </>
     )
